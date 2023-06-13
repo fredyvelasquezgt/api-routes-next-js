2f731cb509e1bdf2db7d0f3c0f3ec21b0f78a8b0
Fredy Velasquez <macacoloco696@gmail.com>
2023-06-12 23:45:52 -0600

    using api routes

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -5,9 +5,10 @@ function buildFeedbackPath() {
   return path.join(process.cwd(), 'data', 'feedback.json')
 }
 
-function extractFeedback() {
+function extractFeedback(filePath) {
   const fileData = fs.readFileSync(filePath)
   const data = JSON.parse(fileData)
+  return data;
 }
 
 function handler(req, res) {
