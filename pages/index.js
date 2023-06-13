ae2610b97418c1618e24aaa499cc88ae6517b35f
Fredy Velasquez <macacoloco696@gmail.com>
2023-06-12 21:47:14 -0600

    refs added

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,21 @@
+import {useRef} from 'react';
+
 function HomePage() {
+
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
   return (
     <div>
       <h1>Home Page</h1>
       <form>
         <div>
           <label htmlFor="email">Your Email Address</label>
-          <input type="email" id="email" />
+          <input type="email" id="email" ref={emailInputRef} />
         </div>
         <div>
           <label htmlFor="feedback">Your Email Address</label>
-          <textarea id="feedback" rows="5"></textarea>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef} ></textarea>
         </div>
         <button>Send Feedback</button>
 
