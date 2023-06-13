b86d8d78f6f4a9287056c4f618e2900d00ef9ce5
Fredy Velasquez <macacoloco696@gmail.com>
2023-06-12 23:00:22 -0600

    finished request

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -16,9 +16,13 @@ function handler(req, res) {
     const filePath = path.join(process.cwd(), 'data', 'feedback.json')
     const fileData = fs.readFileSync(filePath)
     const data = JSON.parse(fileData)
+    data.push(newFeedback);
+    fs.writeFileSync(filePath, JSON.stringify(data));
+    res.status(201).json({message: 'success!', feedback: newFeedback})
 
+  } else {
+    res.status(200).json({message: 'this works'});
   }
-  res.status(200).json({message: 'this works'});
 }
 
 export default handler;
