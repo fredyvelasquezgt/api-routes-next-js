287219b8c2f95bb079d10d17f2f4d9bd07b7f9aa
Fredy Velasquez <macacoloco696@gmail.com>
2023-06-12 22:51:48 -0600

    data dummy file

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,3 +1,6 @@
+import fs from 'fs';
+import path from 'path'
+
 function handler(req, res) {
   if(req.method === 'POST') {
     const email = req.body.email;
@@ -9,7 +12,11 @@ function handler(req, res) {
       text: feedbackText
     };
 
-    
+    //store data in a database or in a file 
+    const filePath = path.join(process.cwd(), 'data', 'feedback.json')
+    const fileData = fs.readFileSync(filePath)
+    const data = JSON.parse(fileData)
+
   }
   res.status(200).json({message: 'this works'});
 }
